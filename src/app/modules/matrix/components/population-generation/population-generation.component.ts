ebdbfc107f0f150fb6c8cacac4535fea46b1b1bd
munko <munko_s@mail.ru>
2022-03-28 21:00:59 +0600

    fix

--- src/app/modules/matrix/components/population-generation/population-generation.component.ts
+++ src/app/modules/matrix/components/population-generation/population-generation.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormArray, FormControl, FormGroup} from "@angular/forms";
+import {MatrixResult, PopulationResult} from "../../models";
 
 @Component({
   selector: 'app-population-generation',
@@ -7,11 +8,12 @@ import {FormArray, FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./population-generation.component.scss']
 })
 export class PopulationGenerationComponent {
-  public matrixIteration:number[][][][] = [];
-
+  public matrixIteration: PopulationResult[] = [];
 
   public form = new FormGroup({
     countColumnCreditUnits: new FormArray([]),
+    bottomCreditUnits: new FormControl(0),
+    topCreditUnits: new FormControl(0),
     countMatrix: new FormControl(0),
     countColumn: new FormControl(0),
     countPopulation: new FormControl(0),
@@ -22,6 +24,14 @@ export class PopulationGenerationComponent {
   public get countColumnCreditUnits(): FormArray { return this.form.get('countColumnCreditUnits') as FormArray; }
   public get countCompetenciesMatrix(): FormArray { return this.form.get('countCompetenciesMatrix') as FormArray; }
 
+  private sum = (previousValue: number, currentValue: number) => previousValue + currentValue;
+  private sumMatrix(matrix:number[][],index: number): number {
+    let sum = 0;
+    matrix.forEach(row=>{
+      sum+=row[index];
+    })
+    return sum;
+  }
   public countColumnChanged($event: Event) {
     const newCountColumn =  +($event.target as any).value;
     this.form.get('countColumn')?.setValue(newCountColumn);
@@ -32,6 +42,23 @@ export class PopulationGenerationComponent {
     }
 
     this.form.setControl('countColumnCreditUnits', new FormArray(newArray))
+  }
+  private sumMatrixCompetencions(competencions: number[],matrix:number[][]): boolean {
+    let sum = 0
+    competencions.forEach((c,index)=>{
+      if(c >= 0.5){
+        sum+= this.sumMatrix(matrix,index);
+      }
+    })
+    return sum >= 1;
+  }
+
+  private closingCompetenciesCalc(firstMatrixRow: number[]): boolean {
+    let sum = 0;
+    firstMatrixRow.forEach((v,i)=>{
+      sum+= v * this.countColumnCreditUnits.value[i];
+    })
+    return sum >= this.form.get('bottomCreditUnits')?.value && this.form.get('topCreditUnits')?.value <= sum;
   }
 
   public countCompetenciesChanged($event: Event) {
@@ -49,7 +76,7 @@ export class PopulationGenerationComponent {
 
 
   public calculation(): void {
-    const resultMatrixArray:number[][][] = [];
+    const resultMatrixArray:MatrixResult[] = [];
     this.matrixIteration = [];
     for(let countMatrix = 0; countMatrix < this.form.get('countMatrix')?.value; countMatrix++){
       const currentMatrix:number[][]  = [];
@@ -65,10 +92,29 @@ export class PopulationGenerationComponent {
         }
         currentMatrix.push(currentRow)
       }
-      resultMatrixArray.push(currentMatrix);
+
+      const firstRowCompetenciesMatrix = (this.countCompetenciesMatrix.value[0] as number[]);
+      resultMatrixArray.push({
+        matrix: currentMatrix,
+        fX: +firstRowCompetenciesMatrix.reduce((previousValue, currentValue,index ) =>{
+          if(currentValue >= 0.5){
+            currentValue * this.sumMatrix(currentMatrix,index);
+          } else {
+            currentValue = 0;
+          }
+          return  previousValue + currentValue;
+        }).toFixed(2),
+        fMax: +firstRowCompetenciesMatrix.filter(x => x >= 0.5).reduce(this.sum).toFixed(2),
+        creditUnits: (this.countCompetenciesMatrix.value as number[][]).every(v=> this.sumMatrixCompetencions(v,currentMatrix)),
+        closingCompetencies: this.closingCompetenciesCalc(currentMatrix[0])
+      });
     }
 
-    this.matrixIteration.push(resultMatrixArray);
+    this.matrixIteration.push({
+      resultMatrixArray: resultMatrixArray
+    });
+
+    console.log( this.matrixIteration);
 
   }
 }
---
+++ src/app/modules/matrix/models.ts
@@ -0,0 +1,13 @@
+export type MatrixResult = {
+  fX: number;
+  fMax: number;
+  closingCompetencies: boolean;
+  creditUnits: boolean;
+  matrix: number[][]
+}
+
+export type PopulationResult = {
+  resultMatrixArray:MatrixResult[];
+};
+
+
